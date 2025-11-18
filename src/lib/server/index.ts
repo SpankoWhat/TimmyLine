ce5af90df5e83e46f6005c3d26aa994c1b49e9ac
Spanko <31244518+SpankoWhat@users.noreply.github.com>
2025-11-17 21:35:23 -0500

    Database Check

--- src/lib/server/index.ts
+++ src/lib/server/index.ts
@@ -1,8 +1,14 @@
 import { drizzle } from 'drizzle-orm/better-sqlite3';
 import Database from 'better-sqlite3';
 import * as schema from './database/index';
+import { logger } from './logging/index.js';
 
-const DATABASE_URL = process.env.DATABASE_URL || 'C:/Users/gwale/Documents/_Projects/TimmyLine/data/timmyD.sqlite';
+const DATABASE_URL = process.env.DATABASE_URL;
+
+if (!DATABASE_URL) {
+    logger.error('DATABASE_URL is not defined in environment variables.');
+    throw new Error('DATABASE_URL is not defined in environment variables.');
+}
 
 const client = new Database(DATABASE_URL);
 export const db = drizzle(client, { schema });
